f238aa1d60f3879635ba8434db0849a52467246d
Mark Gregan <mark@gregan.org>
2017-12-31 15:41:00 -0500

    Updating seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UsersTableSeeder::class,
             BoardsTableSeeder::class,
-            TaskListsTableSeeder::class
+            CardsTableSeeder::class
         ]);
     }
 }
